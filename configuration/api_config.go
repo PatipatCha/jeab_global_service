8906fb4d21b9be8a9fae7eca1a8eac53a9b24e2f
PatipatCha <patipat.jobs@gmail.com>
2023-11-23 23:18:23 +0700

    2318

--- configuration/api_config.go
+++ configuration/api_config.go
@@ -2,7 +2,7 @@ package configuration
 
 func ApiProjectConfig() string {
 	var baseUrl = "https://jeab-project.azurewebsites.net"
-	var apiPath = "/api/get-project"
+	var apiPath = "/api/v1/jguard"
 	var token = "?code=h8lIZJ3SmmTidunpnFLntDrq29ZAK0-QhXKMLX1exD14AzFujfVguA=="
 	res := baseUrl + apiPath + token
 	return res
